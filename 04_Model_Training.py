b4e32a747706447621eeda9d5ead0c8987705532
hdogan84 <56438985+hdogan84@users.noreply.github.com>
2025-01-31 15:32:58 +0100

    cleaned up the training notebook and script

--- 04_Model_Training.py
+++ 04_Model_Training.py
@@ -7,83 +7,84 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 import h5py
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import callbacks, Sequential
+from tensorflow.keras.optimizers import Adam
 
 print(tf.__version__)
 
 from sklearn.model_selection import train_test_split
 
 
-# train = pd.read_csv('train.csv',header=None,index_col=None)
-# train.head()
+train_data_generator = ImageDataGenerator(rescale = 1./255,
+                                   validation_split = 0.2,
+                                   )
+
+training_data  = train_data_generator.flow_from_directory(directory = 'data/train_data/spec',
+                                                   #target_size = (224, 224),
+                                                   class_mode = 'binary',
+                                                   subset = "training", 
+                                                   batch_size = 32)
+
+validation_data  = train_data_generator.flow_from_directory(directory = 'data/train_data/spec',
+                                                   #target_size = (224, 224),
+                                                   class_mode = 'binary',
+                                                   subset = "validation", 
+                                                   batch_size = 32)
 
 
-# np.save('train.npy', train)
-train=np.load('train.npy')
-# print(train.shape)
+early_stopping = callbacks.EarlyStopping(monitor = 'val_loss',
+                                         patience = 4,
+                                         mode = 'min',
+                                         restore_best_weights = True)
 
-y_data=train[:,0]
-# print(y_data.shape)
+lr_plateau = callbacks.ReduceLROnPlateau(monitor = 'val_loss',
+                                        patience=3,
+                                         factor=0.5,
+                                         verbose=2,
+                                         mode='min',
+                                         min_lr = 1e-10)
 
-x_data=train[:,1:]
 
-x_data=x_data.reshape([462,257,395,1])
-
-# do train test split 
-x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.15, random_state=22)
-
-# x_train.shape
-# y_train.shape
-
-inp_shape=(257,395,1)
-
-# yy=tf.keras.layers.Conv2D(32,(3,3),2,input_shape=inp_shape)(x_train)
-# yy.shape
-# zz=tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding='valid', data_format=None)(yy)
-# zz.shape
-# tt=tf.keras.layers.Conv2D(8, (3,3), 2)(zz)
-# tt.shape
-# kk=tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding='valid', data_format=None)(tt)
-# kk.shape
-# tf.keras.layers.Flatten()(kk).shape
+inp_shape=(256,256,3)
 
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Conv2D(32, (3,3), 2,activation='relu',input_shape=inp_shape),
-  tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding='valid', data_format=None),
-  tf.keras.layers.Conv2D(8, (3,3), 2,activation='relu'),
-  tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding='valid', data_format=None),
+  tf.keras.layers.Conv2D(32, (3,3), 2, activation='relu',input_shape=inp_shape),
+  tf.keras.layers.AvgPool2D(pool_size=3, strides=2, padding='valid', data_format=None),
+  tf.keras.layers.Conv2D(64, (3,3), 2, activation='relu'),
+  tf.keras.layers.AvgPool2D(pool_size=3, strides=2, padding='valid', data_format=None),
+  tf.keras.layers.Conv2D(16, (3,3), 2, activation='relu'),
+  tf.keras.layers.AvgPool2D(pool_size=3, strides=2, padding='valid', data_format=None),
   tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(1028, activation='relu'),
+  tf.keras.layers.Dense(1800, activation='relu'),
+  tf.keras.layers.Dense(1264, activation='tanh'),
+  tf.keras.layers.Dense(512, activation='relu'),
+  tf.keras.layers.Dense(256, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dense(16, activation='relu'),
-  tf.keras.layers.Dropout(0.25),
-  tf.keras.layers.Dense(2, activation='softmax')
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
+
+### Example size check
+#x = np.random.rand(1,256, 256, 3) 
+#print(model(x).shape)
+
+optimizer = Adam(learning_rate=0.0001) 
+
+model.compile(optimizer=optimizer,
+              loss='binary_crossentropy',
               metrics=['accuracy'])
 
 
-num_epochs=40
-for ii in range(0,num_epochs):
-    model.fit(x_train, y_train, epochs=1)
-    model.evaluate(x_test,  y_test, verbose=2)
+history = model.fit(training_data, validation_data=validation_data, epochs=20, callbacks=[early_stopping,lr_plateau])
 
 
-model.save('C:/Users/dgnhk_000/Downloads/ARSU 2017/20170330_Uhu/Waldschnepfe_recog/my_model_epochs36_v2.h5')
+#from sklearn.metrics import confusion_matrix
+#confusion_mtx = confusion_matrix(y_test, y_pred) 
+#print(confusion_mtx)
 
-y_predict=model.predict(x_test)
-
-
-y_pred = np.argmax(y_predict, axis=1)
-y_pred
-
-
-from sklearn.metrics import confusion_matrix
-confusion_mtx = confusion_matrix(y_test, y_pred) 
-print(confusion_mtx)
-
-
-model.save('C:/Users/dgnhk_000/Downloads/ARSU 2017/20170330_Uhu/Waldschnepfe_recog/my_model_epochs20.h5')
+model.save('my_model_epochs20.h5')
 
