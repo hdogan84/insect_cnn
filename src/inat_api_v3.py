baa21ea57228e354cc5d796173f7dde7dc2d9fca
Hakan Dogan <dgnhkn@gmail.com>
2025-10-21 11:28:37 +0200

    latest version of api script. Start with CNN experments

--- src/inat_api_v3.py
+++ src/inat_api_v3.py
@@ -12,7 +12,7 @@ import time
 import requests
 import io, os
 from PIL import Image
-
+import pandas as pd
 
 def get_urls_from_api(taxa_id:int, page_num: int = 1, per_page: int = 5):
     """Find all image urls from a given observation page index.
@@ -31,7 +31,7 @@ def get_urls_from_api(taxa_id:int, page_num: int = 1, per_page: int = 5):
         "taxon_id": taxa_id,   
         "per_page": per_page, # Max per page seems to be 200
         "page": page_num,
-        "order_by": "created_at"
+        "quality_grade": "research"
     }
     response = requests.get(url, params=params)
     data = response.json()
@@ -153,23 +153,21 @@ if __name__ == '__main__':
         (124145, 'Xylocopa_violacea')
     ]
 
-    ind_spec = [1036779,1043097,1043717,1047536,1048809,1063891,1071240,1071266,1077829,1310622,1311477,1311527,1311649,1311651,1311671,1311676,1311797,1311798,1311805,1311825,1311831,1311838,1312367,1312773,1315132,1318848,1318874,1329873,1334278,1334493,1334499,1334821,1335321,1335556,1335648,1336116,1336802,1337092,1338442,1338640,1339951,1340286,1340301,1340305,1340342,1340344,1340394,1340405,1340418,1340434,1340503,1340527,1340542,1341976,1342108,1346919,1348620,1349795,1357156,1428217,1495562,1497483,1500013,1502577,1524843,1535521,1535529,1536289,1536449,1536559,1536796,1537212,1537245,1537266,1537412,1537717,1537719,1541217,1541740,1541763,1541799,1541832,1590998,1670212,1873079,2105360,4388646,4452252,4459737,4470628,4485729,4485776,4486826,4493809,4494295,4516754,4518735,4989904,4990191,4990995,4991026,4994132,4994160,5035785,5035857,5037317,5039039,5039096,5039166,5039314,5040875,5063973,5071172,5156102,5742495,5766554,5766556,5871389,5978844,7412043,8703749,9198953,9295353,
-            9711771,11195063,11219325,
-            ]
+    df = pd.read_csv("../webapp_species/gbif_specie_taxon.csv")
 
     page_num = 1
     per_page = 100
 
-    print(len(ind_spec))
+    for ind, row in df.iloc[1:].iterrows():
 
-    for i in range(20, len(ind_spec)):
+        ind = row["taxon_id"]
 
-        ind = ind_spec[i]
+        spec = row["Latine_name"].replace(" ","_")
 
-        print('\n******** ' + str(ind) + ' ********\n')
+        print('\n******** ' + spec + ' ********\n')
         search_and_download(search_ind = ind,
-                            #target_path = 'Z:\data\Bees\\' + spec, 
-                            target_path = '/home/hakandogan/projects/Insect-CNN/data/image_data/',
+                            # target_path = '/home/hakandogan/projects/Insect-CNN/data/image_data/' + spec + '/',
+                            target_path = '/home/hakandogan/projects/Insect-CNN/data/image_data/', # spec name is already inside another function
                             page_num = page_num,
                             number_images = per_page)
 
